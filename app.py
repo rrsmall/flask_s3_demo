92ea88ee1f4b7e08b29819410cbf86ee81c0cc4c
rrsmall <46974871+rrsmall@users.noreply.github.com>
2019-04-17 12:51:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template_string
 from flask_s3 import FlaskS3
+from config import Config
 
 app = Flask(__name__)
-app.config['FLASKS3_BUCKET_NAME'] = 'cus1166-flask-s3-example'
-app.config['AWS_SECRET_KEY_ID'] = 'AWS_SECRET_KEY_ID'
-app.config['AWS_SECRET_ACCESS_KEY'] = 'AWS_SECRET_ACCESS_KEY'
+app.config.from_object(Config)
 s3 = FlaskS3(app)
 
 @app.route('/')
 def index():
-    template_str="""{{ url_for('static', filename="goat.jpg") }}"""
+    template_str="""{{ url_for('static', filename="goat.jpg") }} """
+
     return render_template_string(template_str)
+
+@app.route('/class')
+def index2():
+    template_str="""{{ url_for('static', filename="hopethisworks.jpg") }} """
+
+    return render_template_string(template_str)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5110)
